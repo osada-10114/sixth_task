4b2194083f7e142cc0c389c495eec2c31d1c15e0
TakaoOsada <try-osada@yahoo.ne.jp>
2017-11-18 19:07:39 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :consumers, controllers: {
+    sessions: 'consumers/sessions',
+    registrations: 'consumers/registrations',
+    passwords: 'consumers/passwords'
+  }
+  
   devise_for :users, controllers: {
   	sessions: 'users/sessions',
   	registrations: 'users/registrations',
@@ -9,5 +15,6 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   get 'users/:id' => 'users#show', as: 'user'
+  get 'consumers/:id' => 'consumers#show', as: 'consumer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
